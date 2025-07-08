d478371b8d7f0dbfc49fd84e82926cab4b44d0dd
yulvianiputeri <yulvianipps@students.amikom.ac.id>
2025-07-09 03:26:01 +0700

    update to use folder, no CSV

--- utils.py
+++ utils.py
@@ -1,17 +1,10 @@
-# utils.py
-import cv2
+
+import os
 import numpy as np
-import pandas as pd
-import streamlit as st
+import cv2
+from PIL import Image
 from skimage.feature import local_binary_pattern
 import mahotas
-from PIL import Image
-import plotly.express as px
-import plotly.graph_objects as go
-
-def load_css():
-    with open('styles.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def extract_features_single(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -23,20 +16,15 @@ def extract_features_single(img):
     glcm = mahotas.features.haralick(gray).mean(axis=0)
     return np.hstack([hist, glcm])
 
-@st.cache_data
-def load_dataset_features():
-    df = pd.read_csv("shrimp_dataset.csv")
-    features, labels, paths = [], [], []
-    for idx, row in df.iterrows():
-        img = Image.open(row['image_path']).convert('RGB').resize((128,128))
-        img = np.array(img)
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        lbp = local_binary_pattern(gray, 24, 3, 'uniform')
-        hist, _ = np.histogram(lbp, bins=np.arange(0, 27), range=(0, 26))
-        hist = hist.astype("float")
-        hist /= (hist.sum() + 1e-6)
-        glcm = mahotas.features.haralick(gray).mean(axis=0)
-        features.append(np.hstack([hist, glcm]))
-        labels.append(0 if row['label']=="Healthy" else 1)
-        paths.append(row['image_path'])
-    return np.array(features), np.array(labels), paths
+def load_gallery_images(base_folder, label, sample_size=6):
+    images = []
+    folder_path = os.path.join("data_udang", base_folder)
+    files = os.listdir(folder_path)[:sample_size]
+    for fname in files:
+        img_path = os.path.join(folder_path, fname)
+        try:
+            img = Image.open(img_path).convert('RGB').resize((128,128))
+            images.append((img, label))
+        except:
+            continue
+    return images
